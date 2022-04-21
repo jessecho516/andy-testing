054797d5554f0dc9a1ad870ff18019a69f3d8ec1
havzor1231 <59190208+havzor1231@users.noreply.github.com>
2022-04-21 19:56:33 -0400

    Render search results w/ correct data

--- app.py
+++ app.py
@@ -1,7 +1,69 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, session, redirect
 from webforms import SearchForm
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from search import filter_search
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from dotenv import find_dotenv, load_dotenv
+from authlib.integrations.flask_client import OAuth
+from os import environ as env
+from urllib.parse import quote_plus, urlencode
+
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
 app = Flask(__name__)
+app.secret_key = env.get("APP_SECRET_KEY")
+
+oauth = OAuth(app)
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
+
+db_name = "database.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + db_name
+app.config["SECRET_KEY"] = "abcde"
+
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<Name %r>" % self.name
+
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+class Calendars(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    classes = db.relationship("Courses")
+
+class Courses(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    class_code = db.Column(db.String(100), unique=True)
+    class_title = db.Column(db.String(150), unique=True)
+    credit_hours = db.Column(db.Integer)
+    times = db.Column(db.String(100)) 
+    professor_name = db.Column(db.String(100))
+
+
+
 
 @app.route('/')
 def index():
@@ -17,7 +79,30 @@ def about():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://" + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("home", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
 
 #Pass things to index_base.html
 @app.context_processor
@@ -27,10 +112,12 @@ def base():
 
 @app.route("/search", methods = ["POST"])
 def search():
+    class_list = []
     if request.method == "POST":
         form = SearchForm()
         searched = form.searched.data
         filtered = filter_search(searched)
+
         return render_template("search.html", searched=searched, form = form, filtered = filtered)
 
 
--- search.py
+++ search.py
@@ -3,6 +3,16 @@ import os
 current_file_path = __file__
 current_file_dir = os.path.dirname(__file__)
 class_json_path = os.path.join(current_file_dir, "json", "classes.json")
+ratings_json_path = os.path.join(current_file_dir, "json", "Professor_Ratings.json")
+
+class CourseInfo:
+    def __init__(self, time, course_name_section, professor, days_of_week, section_number, prof_rating):
+        self.time = time
+        self.course_name_section = course_name_section
+        self.professor = professor
+        self.days_of_week = days_of_week
+        self.section_number = section_number
+        self.prof_rating = prof_rating
 
 def make_class_list(classesJson):
     class_file = open(classesJson)
@@ -17,15 +27,41 @@ def make_class_list(classesJson):
     return class_list
 
 
-def search_class(prefix, class_list):
+def search_class(prefix, class_list, classesJson, ratingsJson):
+    class_file = open(classesJson)
+    ratings_file = open(ratingsJson)
+    data = json.load(class_file)
+    ratings_data = json.load(ratings_file)
+
     results = []
     for curr in class_list:
-        class_name = curr[0]
-        if class_name.upper().startswith(prefix.upper()):
-            results.append(curr)
+        class_code = curr[0]
+        #if search matches, get additional information
+        if class_code.upper().startswith(prefix.upper()):
+            course_name = data[class_code]["Class Title"]
+            professor = data[class_code]["Professors"]
+
+            if ratings_data.get(professor):
+                prof_rating = ratings_data[professor]["Level of Difficulty"]
+            else:
+                prof_rating = "N/A"
+            
+            for section in data[class_code]["Sections"]:
+                string_time = section["Times"].split(" ")
+                # print(string_time)
+
+                days_of_week = string_time[0]
+                # print(days_of_week)
+                curr_time = string_time[1]
+                section_number = section["Section Number"]
+                course_name_section = class_code + "-" + section_number + ": " + course_name 
+
+                curr_course = CourseInfo(curr_time, course_name_section, professor, days_of_week, section_number, prof_rating)
+                results.append(curr_course)
     return results
 
 def filter_search(searched):
     class_list = make_class_list(class_json_path)
-    results = search_class(searched, class_list)
+    results = search_class(searched, class_list, class_json_path, ratings_json_path)
+
     return results
